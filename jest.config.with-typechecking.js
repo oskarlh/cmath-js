89bd6330ec3a0da998488fcd21bc19befc4f8a00
Oskar Larsson Högfeldt <oskar@oskar.pm>
2023-10-21 20:23:33 +0200

    Require 100% test coverage

--- jest.config.with-typechecking.js
+++ jest.config.with-typechecking.js
@@ -3,6 +3,16 @@ import withoutTypechecking from "./jest.config.js";
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
 	...withoutTypechecking,
+	collectCoverage: true,
+	coverageReporters: ["text"],
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100,
+		},
+	},
 	transform: {
 		"^.+\\.ts$": ["ts-jest"],
 	},
