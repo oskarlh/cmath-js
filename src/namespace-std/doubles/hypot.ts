8b78ba05ca09dce4821746e90a133419b7d41418
Oskar Larsson Högfeldt <oskar@oskar.pm>
2026-04-23 22:27:07 +0200

    Add JSDoc comments to `hypot()`

--- src/namespace-std/doubles/hypot.ts
+++ src/namespace-std/doubles/hypot.ts
@@ -1,12 +1,18 @@
-// hypot computes the square root of the sum of the squares of x and y, without undue overflow or underflow
-// This implementation allows an optional third argument, as specified in the C++ standard
-// C spec: https://web.archive.org/web/20181230041359if_/http://www.open-std.org/jtc1/sc22/wg14/www/abq/c17_updated_proposed_fdis.pdf#subsection.7.12.7
-// C spec: https://web.archive.org/web/20181230041359if_/http://www.open-std.org/jtc1/sc22/wg14/www/abq/c17_updated_proposed_fdis.pdf#subsection.13.10.4
-// C++ spec for 3-arg version: https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2017/n4659.pdf#subsection.29.9.3
-// Cppreference C version (limited to 2 args): https://en.cppreference.com/w/c/numeric/math/hypot
-// Cppreference C++ version (2 or 3 args): https://en.cppreference.com/w/cpp/numeric/math/hypot
-// ECMAScript's Math.hypot: https://www.ecma-international.org/ecma-262/9.0/index.html#sec-math.hypot
-// Complicated by the requirements for implementations for IEC 60559 floating-point environments, which thankfully only apply to the 2-arg (C) version
+/**
+ * Computes the square root of the sum of the squares of `x` and `y`,
+ * without undue overflow or underflow.
+ *
+ * This implementation allows an optional third argument, as specified in the C++ standard.
+ * The implementation is complicated by the requirements for implementations in IEC 60559
+ * floating-point environments, which thankfully only apply to the 2-arg (C) version.
+ *
+ * Read more about the original function on
+ * - {@link https://en.cppreference.com/w/c/numeric/math/nan|Cppreference}
+ * - {@link https://web.archive.org/web/20181230041359if_/http://www.open-std.org/jtc1/sc22/wg14/www/abq/c17_updated_proposed_fdis.pdf#subsection.7.12.7|The C23 final draft specification, chapter 7}
+ * - {@link https://web.archive.org/web/20181230041359if_/http://www.open-std.org/jtc1/sc22/wg14/www/abq/c17_updated_proposed_fdis.pdf#subsection.13.10.4|The C23 final draft specification, chapter 13}
+ * - {@link https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2023/n4928.pdf#subsection.28.7.3|The C++23 final draft specification} for the 3-arg version
+ * @returns The hypothenuse
+ */
 export function hypot(/*double*/ x: number, /*double*/ y: number, /*double*/ z?: number): number {
 	if (z !== undefined) {
 		// 3-arg version which has fewer requirements
