720377098204f9dcf6e70c94a9e2ce678752ea3d
Oskar Larsson Högfeldt <oskar@oskar.pm>
2026-04-25 18:29:30 +0200

    Restore `hypot()` tests

--- src/namespace-std/doubles/hypot.test.ts
+++ src/namespace-std/doubles/hypot.test.ts
@@ -1,6 +1,13 @@
+import { expect } from "chai";
 import { hypot } from "./index.ts";
 import { describe, it } from "node:test";
 
 describe(hypot.name, () => {
-	it("computes the square root of the sum of the squares of x and y, without undue overflow or underflow", () => {});
+	it("computes the square root of the sum of the squares of x and y, without undue overflow or underflow", () => {
+		expect(hypot(105, 100)).to.be.closeTo(145, 3);
+		expect(hypot(-Infinity, 0)).to.equal(Infinity);
+		expect(hypot(0, -0.1451493619437592)).to.equal(0.1451493619437592);
+		expect(hypot(0, 0, -0.1451493619437592)).to.equal(0.1451493619437592);
+		expect(hypot(-3, -2, -1)).to.be.closeTo(3.741657386773941, 3);
+	});
 });
